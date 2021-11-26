04845b326bda8817a6b1cc70dc684f5c17a382f8
Yunus AK <yunus.ak.gen.tr@gmail.com>
2021-11-26 17:37:21 +0300

    added TaskListBuilder

--- lib/features/task/presentation/widgets/task_list.dart
+++ lib/features/task/presentation/widgets/task_list.dart
@@ -4,6 +4,7 @@ import 'package:todoey/features/task/domain/entities/task.dart';
 import 'package:todoey/features/task/presentation/bloc/events.dart';
 import 'package:todoey/features/task/presentation/bloc/states.dart';
 import 'package:todoey/features/task/presentation/bloc/task_bloc.dart';
+import 'package:todoey/features/task/presentation/widgets/task_list_builder.dart';
 import 'package:todoey/features/task/presentation/widgets/task_tile.dart';
 
 class TaskListView extends StatelessWidget {
@@ -23,23 +24,7 @@ class TaskListView extends StatelessWidget {
           return Text("Ops! Can't load tasks.");
         } else if (state is TasksLoaded) {
           return (state.tasks.length > 0)
-              ? ListView.builder(
-                  itemBuilder: (context, index) {
-                    final currentTask = state.tasks[index];
-                    return TaskTile(
-                      task: currentTask,
-                      checkboxCallBack: (bool checkboxState) {
-                        final updatedTask = TodoTask(
-                          id: currentTask.id,
-                          name: currentTask.name,
-                          isDone: checkboxState,
-                        );
-                        context.read<TaskBloc>().add(UpdateTask(updatedTask));
-                      },
-                    );
-                  },
-                  itemCount: state.tasks.length,
-                )
+              ? TaskListBuilder(state.tasks)
               : Text('No tasks :)');
         }
         return Text('Unknown State!');
---
+++ lib/features/task/presentation/widgets/task_list_builder.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/features/task/domain/entities/task.dart';
+import 'package:todoey/features/task/presentation/bloc/events.dart';
+import 'package:todoey/features/task/presentation/bloc/task_bloc.dart';
+
+import 'package:todoey/features/task/presentation/widgets/task_tile.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TaskListBuilder extends StatelessWidget {
+  final List<TodoTask> tasks;
+  const TaskListBuilder(
+    this.tasks, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final currentTask = tasks[index];
+        return TaskTile(
+          task: currentTask,
+          checkboxCallBack: (bool checkboxState) {
+            final updatedTask = TodoTask(
+              id: currentTask.id,
+              name: currentTask.name,
+              isDone: checkboxState,
+            );
+            context.read<TaskBloc>().add(UpdateTask(updatedTask));
+          },
+        );
+      },
+      itemCount: tasks.length,
+    );
+  }
+}
